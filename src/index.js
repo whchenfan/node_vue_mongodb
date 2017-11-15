f97245da2e1cef1105a8f7eb0e6bfd423a02a854
whchenfan <1060927555@qq.com>
2017-11-15 10:56:42 +0800

    前端入口文件

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import axios from 'axios'
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+Vue.config.silent  = false;
+Vue.config.devtools = false;
+Vue.config.productionTip = false;
+Vue.prototype.$ajax=axios;
+import commFn from './js/commJs';
+Vue.use(commFn);
+//导入视图路由
+import routes from './js/router';
+const router=routes(VueRouter);
+new Vue({
+    router,
+    el: '#app'
+})
